7048516c17f8b78d85ed5a27f691f49e79230b19
Michael <michael@archsmarter.com>
2022-09-23 15:32:44 -0400

    done

--- RAASession07Bonus/Command.cs
+++ RAASession07Bonus/Command.cs
@@ -25,32 +25,14 @@ namespace RAASession07Bonus
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            TaskListWindow curForm = new TaskListWindow();
+            curForm.Width = 500;
+            curForm.Height = 700;
 
-            Selection sel = uidoc.Selection;
+            curForm.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            curForm.Topmost = true;
 
-            // Retrieve elements from database
-
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
-            {
-                Debug.Print(e.Name);
-            }
-
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }
+            curForm.ShowDialog();
 
             return Result.Succeeded;
         }
---
+++ RAASession07Bonus/TaskListWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Autodesk.Revit.UI;
+
+namespace RAASession07Bonus
+{
+    /// <summary>
+    /// Interaction logic for TaskListWindow.xaml
+    /// </summary>
+    public partial class TaskListWindow : Window
+    {
+        public TaskListWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddEdit_Click(object sender, RoutedEventArgs e)
+        {
+            lbxTasks.Items.Add(tbxItem.Text);
+            tbxItem.Text = "";
+        }
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            TaskDialog.Show("test", "Clicked up button");
+        }
+
+        private void btnDn_Click(object sender, RoutedEventArgs e)
+        {
+            TaskDialog.Show("test", "Clicked down button");
+        }
+
+        private void lbxTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TaskDialog.Show("test", "I selected an item");
+        }
+    }
+}
